b03e0a1d098f0c3efa965c38cc196dd48bf27bbc
Santhosh Kumar A L <santhosh.al@robosoftin.com>
2020-12-02 12:03:18 +0530

    add the readme file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # starwars-names
+
+# installation
+
+This package is distributed via npm:
+
+npm install starwars-names
+# usages
+var names = require('starwars-names');
+var allNames = names.all;
+var randomName = names.random();
+var threeRandomNames = names.random(3);
+Other
+This library was developed by me as part of an egghead.io series called "How to Write a JavaScript Library."
+
+I'm now also using it to give a workshop for Frontend Masters.
+
+# Project Setup
+This project assumes you have NodeJS v6 or greater installed. You should also have npm v3 or greater installed as well (this comes packaged with Node 6). You'll also need a recent version of git installed as well.
+
+You may have come to this project from different varying sources. There are a different series of branches for each workshop/course I've done. To get started with the project, start with this:
+
+
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,12 @@
 //import {expect} from 'chai';
 //import starWars from './index';
 var expect = require('chai').expect;
-var starwars = require('./index')
+var starWars = require('./index')
 
 describe('starwars-names', function() {
   describe('all', function() {
     it('should be an array of strings', function() {
-      expect(starwars.all).to.satisfy(isArrayOfStrings);
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
 
       function isArrayOfStrings(array) {
         return array.every(function(item) {
@@ -16,15 +16,22 @@ describe('starwars-names', function() {
     });
 
     it('should contain `Luke Skywalker`', function() {
-      expect(starwars.all).to.include('Luke Skywalker');
+      expect(starWars.all).to.include('Luke Skywalker');
     });
   });
 
   describe('random', function() {
     it('should return a random item from the starWars.all', function() {
-      var randomItem = starwars.random();
-      expect(starwars.all).to.include(randomItem);
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
     });
 
+    it('should return an array of random items if passed a number', function() {
+      var randomItems = starWars.random(3);
+      expect(randomItems).to.have.length(3);
+      randomItems.forEach(function(item) {
+        expect(starWars.all).to.include(item);
+      });
+    });
   });
 });
